557d6f9c950d98a8d06eef3ef813d897f05414ca
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-10 07:32:18 -0500

    Se creo la base de datos SQLite en la carpeta Data tambien se añadio los metodos de crear tabla, insertar, update, delete, get para conseguir la lista de dispositivos, y en este caso un get por Id para la parte de editar y eliminar dispositivos registrados

---
+++ MaquisKExamP3ControlEquiElectro/Data/AppDataBase.cs
@@ -0,0 +1,36 @@
+﻿using MaquisKExamP3ControlEquiElectro.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquisKExamP3ControlEquiElectro.Data
+{
+    public class AppDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public AppDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Equipo>().Wait();
+        }
+
+        public Task<int> InsertEquipoAsync(Equipo equipo) =>
+            _database.InsertAsync(equipo);
+
+        public Task<int> UpdateEquipoAsync(Equipo equipo) =>
+            _database.UpdateAsync(equipo);
+
+        public Task<int> DeleteEquipoAsync(Equipo equipo) =>
+            _database.DeleteAsync(equipo);
+
+        public Task<List<Equipo>> GetEquiposAsync() =>
+            _database.Table<Equipo>().ToListAsync();
+
+        public Task<Equipo> GetEquipoByIdAsync(int id) =>
+            _database.Table<Equipo>().Where(e => e.Id == id).FirstOrDefaultAsync();
+    }
+}
