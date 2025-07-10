a7b8b554646df576f01f21cf6d6678089c83f2db
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-10 08:27:56 -0500

    Se configuro la navegación con Shell y TabBar para Registro, Lista y Logs ademas, se añadio la estructura de navegación utilizando Shell con tres pestañas principales: Registro, Lista de Dispositivos  y Ver Logs. Se integraron las vistas correspondientes (`RegistroPage`, `ListaPage`, `LogsPage`) para facilitar una navegación fluida entre los módulos principales de la aplicación. También se adaptó la clase App para establecer `AppShell` como la página principal.

--- MaquisKExamP3ControlEquiElectro/AppShell.xaml.cs
+++ MaquisKExamP3ControlEquiElectro/AppShell.xaml.cs
@@ -1,10 +1,37 @@
-﻿namespace MaquisKExamP3ControlEquiElectro
+﻿using MaquisKExamP3ControlEquiElectro.Views;
+using Microsoft.Maui.Controls;
+
+namespace MaquisKExamP3ControlEquiElectro
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        public AppShell(RegistroPage registroPage, ListaPage listaPage, LogsPage logsPage)
         {
             InitializeComponent();
+
+            Items.Clear();
+
+            Items.Add(new TabBar
+            {
+                Items =
+                {
+                    new ShellContent
+                    {
+                        Title = "Registrar",
+                        Content = registroPage
+                    },
+                    new ShellContent
+                    {
+                        Title = "Lista de Dispositivos",
+                        Content = listaPage
+                    },
+                    new ShellContent
+                    {
+                        Title = "Ver Logs",
+                        Content = logsPage
+                    }
+                }
+            });
         }
     }
 }
