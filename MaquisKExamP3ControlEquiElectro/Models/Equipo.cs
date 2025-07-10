16104cabe0d0436369f42698aa6d9bb41d8c6081
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-10 07:26:44 -0500

    Se creo el modelo principal, en este caso "equipo", Se añandio Id, Dispositivo, Marca, Garantia y VidaUtilMeses con sus respectivos tipo de datos.

---
+++ MaquisKExamP3ControlEquiElectro/Models/Equipo.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquisKExamP3ControlEquiElectro.Models
+{
+    public class Equipo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Dispositivo { get; set; }
+        [MaxLength(100)]
+        public string Marca { get; set; }
+        public bool GarantiaActiva { get; set; }
+        public int VidaUtilMeses { get; set; }
+    }
+}
