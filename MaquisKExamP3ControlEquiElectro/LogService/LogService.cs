395484046ba48689d0f729f12bbfa1210ec9fb60
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-10 07:47:32 -0500

    Se creo la sevicio LogService para permitir registrar no solo el nombre del dispositivo, sino también el tipo de acción realizada por ejemplo, Creo, Actualizo o Elimino,  el método AppendLogAsync se añadio un parámetro "accion" que se incluye en el mensaje del log, manteniendo el formato de fecha y hora. Esto facilita el seguimiento de cambios realizados por el usuario, manteniendo la persistencia de los logs en archivos .txt dentro del directorio local del proyecto.

---
+++ MaquisKExamP3ControlEquiElectro/LogService/LogService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquisKExamP3ControlEquiElectro.LogService
+{
+    public static class LogService
+    {
+        private static string apellido = "Maquis";
+
+        public static async Task AppendLogAsync(string dispositivo, string accion)
+        {
+            string filename = $"Logs_{apellido}.txt";
+            string logPath = Path.Combine(FileSystem.AppDataDirectory, filename);
+            string logEntry = $"Se {accion} el registro [{dispositivo}] el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            await File.AppendAllTextAsync(logPath, logEntry);
+        }
+
+        public static async Task<List<string>> LeerLogsAsync()
+        {
+            string filename = $"Logs_{apellido}.txt";
+            string logPath = Path.Combine(FileSystem.AppDataDirectory, filename);
+            if (!File.Exists(logPath))
+                return new List<string>();
+
+            var lines = await File.ReadAllLinesAsync(logPath);
+            return lines.ToList();
+        }
+    }
+}
