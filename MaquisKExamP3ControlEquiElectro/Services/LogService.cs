80438527a5adb634029030e7fd549aef3a781da9
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-10 08:02:50 -0500

    Se movio LogService a otra carpeta porque daba un error de referencia y  se creo el LogViewModel donde se agrego el codigo para cargar los logs  desde el LogService y posterior esto mostrar en la vista de logs

--- MaquisKExamP3ControlEquiElectro/Services/LogService.cs
+++ MaquisKExamP3ControlEquiElectro/Services/LogService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MaquisKExamP3ControlEquiElectro.LogService
+namespace MaquisKExamP3ControlEquiElectro.Services
 {
     public static class LogService
     {
---
+++ MaquisKExamP3ControlEquiElectro/ViewModels/LogViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MaquisKExamP3ControlEquiElectro.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquisKExamP3ControlEquiElectro.ViewModels
+{
+    public class LogsViewModel : BaseViewModel
+    {
+        private ObservableCollection<string> logs;
+        public ObservableCollection<string> Logs
+        {
+            get => logs;
+            set { logs = value; OnPropertyChanged(); }
+        }
+
+        public LogsViewModel()
+        {
+            _ = CargarLogsAsync();
+        }
+
+        public async Task CargarLogsAsync()
+        {
+            var registros = await LogService.LeerLogsAsync();
+            Logs = new ObservableCollection<string>(registros);
+        }
+    }
+}
